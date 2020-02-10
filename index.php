a59103ca608788539446d5caf2a3dc88a39fc3c8
Pieterjan van Dijk <Pieterjan.v.dijk@gmail.com>
2020-02-10 14:45:18 +0100

    starting

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Stemwijzer bitfchesss</title>
+	<link rel="stylesheet" type="text/css" href="styling/styling.css">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+	<script src="http://stemwijzer.dvc-icta.nl/data.js"></script>
+	<script type="text/javascript" src="script/script.js"></script>
+
+
+	<div id='main-content'>
+		<div id='main-content-content'>
+			<img src="img/stemwijzer-logo.png">
+			<h2 id='stemwijzer-logo-text'>StemWijzer Tweede Kamer 2020</h2>
+			<h1 id='stemwijzer-logo-header'>Test uw politieke voorkeur aan de hand van 30 stellingen</h1>
+			<div id='main-content-startdiv'>
+				<div id='main-content-startdiv-subdiv'>
+					<span class="main-content-span" id='main-content-span-1'>
+						<img class='start-page-icons' id='start-page-icon-1' src="img/person-circle.png">
+						<p>Bekijk de toegankelijke versie voor mensen met een visuele beperking</p>	
+					</span>
+					<span class="main-content-span" id='main-content-span-2'>
+						<img class='start-page-icons' id='start-page-icon-2' src="img/lock.svg">
+						<p>De StemWijzer maakt geen gebruik van cookies. Klik hier voor de volledige privacy statement</p>	
+					</span>
+				</div>
+				<span id='main-content-button-span'>
+					<button onclick="alert('nothing')">Start</button>
+				</span>
+			</div>
+			<div id='main-content-bottom'>
+				<a href="index.php">Of blik terug met de <span>Stemmen</span>Tracker</a>
+				<p>Wat hebben de partijen in het verleden waargemaakt van hun verkiezingsbeloften? 'Track' uw stem met de Stemmentracker, een website die terugblikt op 30 Tweede Kamerstemmingen van de afgelopen regeerperiode. Met welke partij stemde u mee?</p>
+			</div>
+		</div>
+		<div id='main-content-parties'>
+			<p>main-content-parties</p>
+		</div>
+	</div>
+</body>
+</html>
